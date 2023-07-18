0cf7018a5487a63d9d483f2df8d426e0fff1be93
Mpho <mphongwenya96@gmail.com>
2023-07-18 08:56:19 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 0 ; m < 10 ; m++)
+		putchar(m + '0');
+	putchar('\n');
+	return (0);
+}
