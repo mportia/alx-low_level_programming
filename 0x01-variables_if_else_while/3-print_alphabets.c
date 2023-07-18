eb1961fadea283775a29e903611b74605ab55ee3
Mpho <mphongwenya96@gmail.com>
2023-07-18 05:26:03 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char a;
+
+	char b;
+
+	a = 'a';
+	b = 'A';
+	while
+		(a <= 'z') {
+			putchar(a);
+			c++;
+		}
+	while
+		(b <= 'z') {
+			putchar (d);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
