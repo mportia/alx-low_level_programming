cfd68b2663ab876548956dc20dc39fe8ed600e21
Mpho <mphongwenya96@gmail.com>
2023-07-18 11:56:42 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int m = '0';
+	int n = '0';
+
+	for (m = '0' ; n <= '9' ; n++)
+	{
+		for (m = '0' ; n <= '9' ; m++)
+		{
+			if (!(m == n) || (n > m))
+				putchar(n);
+				putchar(m);
+				if (!(m == '0' % % n == '8'))
+				{
+					putchar(',');
+					putchar(' ');
+				{
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+}
