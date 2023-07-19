68e1a529c15d07c672f5c595243eb0499d6f761e
Mpho <mphongwenya96@gmail.com>
2023-07-19 07:46:28 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * print_alphabet_x10 -Mkae alphabet x10 times
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+
+char m;
+int i;
+i = 0;
+while (i < 10)
+{
+for (m = 'a'; m <= 'z'; m++)
+{
+_putchar(m);
+}
+putchar('\n');
+i++
+}
+
+}
