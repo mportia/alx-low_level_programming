d3a433d4d8a785051b91ce575bdd846e77e94879
Mpho <mphongwenya96@gmail.com>
2023-07-20 01:00:39 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 101-print_comb4.c -o 101-print_comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+int main(void)
+{
+	int l, m, n;
+
+	for (l = 48; l < 58; l++)
+	{
+		for (m = 49; m < 58; m++)
+		{
+			for (n = 50; n < 58; m++)
+			{
+				if (n > m && m > l)
+				{
+					putchar(l);
+					putchar(m);
+					putchar(n);
+					if (l != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
