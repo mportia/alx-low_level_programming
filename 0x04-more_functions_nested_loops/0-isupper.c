09d5a84edf2d4bbd48ae38fbe48666e44ff7f932
Mpho <mphongwenya96@gmail.com>
2023-07-21 17:15:04 -0700

    Fixed compile error on check 3

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * _isupper - Checks for lower case character
- * @c: The character to be checked
- * Return: 1 for uppercase or 0 for anything els
+ * _isupper - uppercase letters
+ * @c: character check
+ *
+ * Return: 0 or 1
  */
+
 int _isupper(int c)
 {
-if (c >= 65 && c <= 90)
-{
-return (1);
-}
-return (0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
