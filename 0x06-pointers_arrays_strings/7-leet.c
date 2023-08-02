c70a80672c15a45b0448f6e261daa38b12c23158
Mpho <mphongwenya96@gmail.com>
2023-08-02 01:41:46 -0700

    leet writing

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encode into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+
+char *leet(char *n)
+{
+	int m, x;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (m = 0; n[m] != '\0'; m++)
+	{
+		for (x = 0; x < 10; x++)
+		{
+			if (n[m] == s1[x])
+			{
+				n[m] = s2[x];
+			}
+		}
+	}
+	return (n);
+}
